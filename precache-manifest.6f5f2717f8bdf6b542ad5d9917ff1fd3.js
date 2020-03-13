27a4d24179d8888d27193b1030beb9f5bf0ba7de
Витюк Александра <vituks@yandex.ru>
2020-03-13 14:59:18 +0300

    Updates

---
+++ precache-manifest.6f5f2717f8bdf6b542ad5d9917ff1fd3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f32a3b2c26442e1e30aa81c107904609",
+    "url": "/vkminiapps/index.html"
+  },
+  {
+    "revision": "c81d9369b07ebe46c99e",
+    "url": "/vkminiapps/static/css/2.733ba2d0.chunk.css"
+  },
+  {
+    "revision": "c89921d8a5e5e2839510",
+    "url": "/vkminiapps/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "c81d9369b07ebe46c99e",
+    "url": "/vkminiapps/static/js/2.ce455c21.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/vkminiapps/static/js/2.ce455c21.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c89921d8a5e5e2839510",
+    "url": "/vkminiapps/static/js/main.7f23156c.chunk.js"
+  },
+  {
+    "revision": "f942750e0f5fe8b65307",
+    "url": "/vkminiapps/static/js/runtime-main.3ec15ea5.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/vkminiapps/static/media/persik.4e1ec840.png"
+  }
+]);
